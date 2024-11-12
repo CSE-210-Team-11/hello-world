7e25d281289ac224a56501ca4213ebc66aecafb3
SirKanaad26 <kaninamerica@gmail.com>
2024-11-11 19:55:35 -0800

    Fixing jest setup.

--- jest.setup.js
+++ jest.setup.js
@@ -1,10 +1,9 @@
-// src/__tests__/example.test.js
-test('adds 1 + 2 to equal 3', () => {
-  expect(1 + 2).toBe(3);
-});
+// jest.setup.js
 
-test('object assignment', () => {
-  const data = {one: 1};
-  data['two'] = 2;
-  expect(data).toEqual({one: 1, two: 2});
-});
+// Simple mock for fetch API
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    json: () => Promise.resolve({}),
+    ok: true
+  })
+);
